aa7d8c7dbf58890dbbe84747562d243eff425ef5
aum singhal <aumi@aums-Laptop.local>
2024-04-03 19:36:24 +0530

    added homeage video

--- src/components/homepage/hero/hero.jsx
+++ src/components/homepage/hero/hero.jsx
@@ -1,6 +1,7 @@
 import "./hero.css";
 import arrow from "../../../assets/arrow_outward.png";
 import { Fade } from "react-awesome-reveal";
+import landingVideo from "../../../assets/landing-video.mp4";
 
 export default function Hero(){
   return <div className="hero">
@@ -10,7 +11,7 @@ export default function Hero(){
     </div>
 
     <div className="video-container">
-      
+      <video type="video/mp4" muted controls autoPlay={true} loop={true} id="landing-video" src={landingVideo} ></video>
     </div>
 
     <div className="story flex-d-col flex-a-cen">
