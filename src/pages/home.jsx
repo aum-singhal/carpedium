487b0a9c774d0c0351b09edf9fa37a548b233525
aum singhal <aumi@aums-Laptop.local>
2024-04-04 17:54:20 +0530

    added service and about content

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import Hero from "../components/homepage/hero/hero";
 import Team from "../components/homepage/team/team";
 
 export default function HomePage(){
-  const aboutDesc = "Our passion is to bring your narratives to life in the most imaginative and captivating ways. We are a vibrant collective of creative professionals - writers, producers, directors and design artists  united by our love for good storytelling. Our vision is to become your trusted partners, help you  navigate the ever evolving digital landscape and deliver excellent products for all your  communication needs."
+  const aboutDesc = "Welcome to the captivating realm of Carpe Diem Media, where storytelling takes center stage. Picture a  lively crew of visionary dream weavers – writers, producers, directors, and design wizards – all fuelled by a  passion for pushing the boundaries of imagination. Our mission is simple: to be your loyal companions as  we navigate the exhilarating twists and turns of the creative process, producing nothing but awe-inspiring  content for all your communication needs. So, let&#39;s dive headfirst into this whirlwind adventure where  imagination runs wild and every project is an epic tale waiting to be told!"
   return <div className="home-page">
     <Hero />
     <BlueBox title={"ABOUT US"} id={"about"} desc={aboutDesc} />
